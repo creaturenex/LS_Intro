04341aff2a08242c8850741caf6a1bc8c23ee44b
Oscar Romero <oscar.m.romero87@gmail.com>
2019-01-01 18:48:16 -0500

    Added excercise_7

---
+++ The_Basics/Exercise_7.rb
@@ -0,0 +1,10 @@
+# Exercise 7
+# What does the following error message tell you?
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+puts "Before watching video"
+puts "The following error (see below) tells me that the incorrect closing bracket was used. I know that Hashes use the curly brackets.
+
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+ from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'"
