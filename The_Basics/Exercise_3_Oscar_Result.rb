b4ed77aa6e2da4c66bd9804f636a3f8ce8c31c5e
Oscar Romero <oscar.m.romero87@gmail.com>
2019-01-01 18:46:22 -0500

    deleted LS_result and Oscar_Result as the files were the same as exercise 3 broken up individually

--- The_Basics/Exercise_3_Oscar_Result.rb
+++
@@ -1,8 +0,0 @@
-# Excercise 3
-movies = { "Captain America" => 2011,
-  "Ironman" => 2008,
-  "Hulk" => 2009,
-  "The Avengers" => 2012,
-  "Thor" => 2010,}
-
-  movies.each{ |movie, year| puts year}
