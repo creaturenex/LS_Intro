c6deefcbf1f4b6c64f241158bb4679e47893b3de
Oscar Romero <creaturenex@gmail.com>
2019-08-12 19:40:14 -0400

    For Exercise_6.rb, looked at solution and added review of that code. Finished Exercise_7.rb

--- Hashes/Exercise_7.rb
+++ Hashes/Exercise_7.rb
@@ -7,3 +7,19 @@
 # my_hash2 = {x => "some value"}
 
 # What's the difference between the two hashes that were created?
+
+puts "Code before watching video
+
+my_hash = {x: 'some value'}
+yields {:x=>'some value'}
+
+while
+
+my_hash2 = {x => 'some value'}
+{'hi there'=>'some value'}
+
+In my_hash the :x is expressed as a symbol, hence we see 'x' as the key.
+
+In my_hash2 the x is expressed as a varible because it is missing the ':' and x = 'hi there' so store varible is expressed in the key.
+
+after watching the video, my assesment is inline"
